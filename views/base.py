cb7cccf41defffa23c620a3681a082e7797f1a81
anchit-chandran <anchit97123@gmail.com>
2023-11-16 21:10:39 +0000

    adds base view
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

---
+++ views/base.py
@@ -0,0 +1,63 @@
+# Python imports
+import logging
+import tkinter as tk
+
+from constants import config
+from constants.console_color_codes import PrintColor
+
+class BaseView(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        logging.debug(f"{PrintColor.HEADER}{self.__class__.__name__} created {PrintColor.ENDC}")
+        self.render_nav()
+
+    def render_nav(self) -> None:
+        # Create Navbar
+        self.nav_container = tk.Frame(
+            master=self,
+            width=config.SCREEN_WIDTH,
+            height=20,
+            relief=tk.SUNKEN,
+        )
+        self.nav_container.pack(fill="x", expand=True)
+
+        self.nav_items_left_container = tk.Frame(
+            master=self.nav_container,
+        )
+        self.nav_items_left_container.grid(
+            row=0,
+            column=0,
+            sticky="w",
+        )
+
+        self.all_plans_button = tk.Button(
+            master=self.nav_items_left_container, text="All Plans"
+        )
+        self.all_plans_button.grid(row=0, column=0, sticky="w")
+
+        self.volunteer_list_button = tk.Button(
+            master=self.nav_items_left_container, text="Volunteer List"
+        )
+        self.volunteer_list_button.grid(row=0, column=1, sticky="w")
+
+        self.messages_button = tk.Button(
+            master=self.nav_items_left_container, text="Messages"
+        )
+        self.messages_button.grid(row=0, column=2, sticky="w")
+
+        self.nav_items_right_container = tk.Frame(
+            master=self.nav_container,
+        )
+        self.nav_items_right_container.grid(
+            row=0,
+            column=1,
+            sticky="e",
+        )
+        self.logout_button = tk.Button(
+            master=self.nav_items_right_container,
+            text="Logout",
+        )
+        self.logout_button.pack(
+            side="right",
+            anchor="e",
+        )
