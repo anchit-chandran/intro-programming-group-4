8ca93ad7d4e945213fb196d1d56f629e3ace1d5d
anchit-chandran <anchit97123@gmail.com>
2023-11-18 18:11:20 +0000

    adds template view
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

---
+++ views/_view_template.py
@@ -0,0 +1,37 @@
+"""TEMPLATE FILE FOR MAKING NEW VIEW"""
+# Python imports
+import tkinter as tk
+
+# Project imports
+from .base import BaseView
+
+
+class MessagesView(BaseView):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.render_widgets()
+
+    def render_widgets(self) -> None:
+        """Renders widgets for view"""
+
+        # Create container
+        self.container = tk.Frame(
+            master=self,
+            width=500,
+            height=300,
+        )
+        self.container.pack(
+            fill="both",
+            padx=30,
+            pady=100,
+        )
+
+        # Header
+        self.header = tk.Label(
+            master=self.container,
+            text=f"TEMPLATE VIEW",
+            font=(60),
+        )
+        self.header.pack()
+
