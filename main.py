837944e75e79fb8507223016e3a919cdda4f3080
Yulia Trubacheva <yulitrub@gmail.com>
2023-11-24 16:39:56 +0000

    calculate volunteer age

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class MainApplication(tk.Tk):
 
         self.current_view = None
         # Start at LoginView
-        self.switch_to_view("add_edit_plan")
+        self.switch_to_view("login")
 
     def switch_to_view(self, new_view: str) -> None:
         "Helper method to overcome python circular import errors"
