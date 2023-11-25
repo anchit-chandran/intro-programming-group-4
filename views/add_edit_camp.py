b0941a11b6d352e20bb1f04ad9855cc5a703270b
Yulia Trubacheva <yulitrub@gmail.com>
2023-11-25 19:59:53 +0000

    Create top container with placeholder values

--- views/add_edit_camp.py
+++ views/add_edit_camp.py
@@ -53,3 +53,88 @@ class AddEditCampView(BaseView):
             side="left",
         )
 
+        # ------------------------ Top container------------------------------
+        # TO DO: get volunteer ID to get camp id from db
+        # TO DO: retrieve camp info from db and put instead of placeholder
+        # TO DO: get resources list from db and map over them to display data
+        # TO DO: edit handle_send_message to redirect correctly - look up
+
+        self.top_container = tk.Frame(
+            master=self.container,
+            width=700,
+            height=600,
+        )
+        self.top_container.grid(row=1, column=0, padx=30, pady=20, sticky="nsew")
+
+        # info container
+        self.info_container = tk.Frame(
+            master=self.top_container, width=300, height=600, bg="lightgrey"
+        )
+        self.info_container.grid(row=0, column=0, padx=30, pady=20, sticky="nsew")
+
+        # header span 2 columns
+        self.info_header = tk.Label(
+            master=self.info_container, text="INFORMATION", font=42, bg="lightgrey"
+        )
+        self.info_header.grid(
+            row=3, column=0, columnspan=2, pady=20, padx=10, sticky="w"
+        )
+
+        # left label
+        self.location_label = tk.Label(
+            master=self.info_container, text="LOCATION:", font=32, bg="lightgrey"
+        )
+        self.location_label.grid(row=4, column=0, sticky="w", pady=10, padx=10)
+
+        self.max_capacity_label = tk.Label(
+            master=self.info_container, text="MAX CAPACITY:", font=32, bg="lightgrey"
+        )
+        self.max_capacity_label.grid(row=5, column=0, sticky="w", pady=10, padx=10)
+
+        # right info
+        self.location_info = tk.Label(
+            master=self.info_container, text="Placeholder", font=24, bg="lightgrey"
+        )
+        self.location_info.grid(row=4, column=1, sticky="w", pady=10, padx=10)
+
+        self.max_capacity_info = tk.Label(
+            master=self.info_container, text="Placeholder", font=24, bg="lightgrey"
+        )
+        self.max_capacity_info.grid(row=5, column=1, sticky="w", pady=10, padx=10)
+
+        # resources container
+        self.resources_container = tk.Frame(
+            master=self.top_container, width=300, height=600, bg="lightgrey"
+        )
+        self.resources_container.grid(row=0, column=1, padx=30, pady=20, sticky="nsew")
+
+        # header span 2 columns
+        self.resources_header = tk.Label(
+            master=self.resources_container, text="RESOURCES", font=42, bg="lightgrey"
+        )
+        self.resources_header.grid(
+            row=3, column=1, columnspan=2, pady=20, padx=10, sticky="w"
+        )
+
+        # left label
+        # get from db and map over
+        self.resources_label = tk.Label(
+            master=self.resources_container, text="Resource 1:", font=24, bg="lightgrey"
+        )
+        self.resources_label.grid(row=4, column=1, sticky="w", pady=10, padx=10)
+
+        # right info
+        self.resources_info = tk.Label(
+            master=self.resources_container, text="Placeholder", font=16, bg="lightgrey"
+        )
+        self.resources_info.grid(row=4, column=2, sticky="w", pady=10, padx=10)
+
+        # button
+        self.send_message_button = tk.Button(
+            master=self.top_container,
+            text="MESSAGE ADMIN",
+            command=self.handle_send_message,
+            bg="red",
+            fg="white",
+        )
+        self.send_message_button.grid(row=0, column=2, padx=30, pady=20, sticky="ne")
