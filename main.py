10e3f04b901645c220ece60711b05625ee31760e
anchit-chandran <anchit97123@gmail.com>
2023-11-26 11:43:42 +0000

    change main to load camp detail
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- main.py
+++ main.py
@@ -42,13 +42,14 @@ class MainApplication(tk.Tk):
                     "user_id": 1,
                     "username": "admin",
                     "is_admin": 1,
-                    "plan_name": "Plan 0",
+                    "camp_id_to_view": "1",
+                    
                 }
             )
 
         self.current_view = None
         # Start at LoginView
-        self.switch_to_view("login")
+        self.switch_to_view("camp_detail")
 
     def switch_to_view(self, new_view: str) -> None:
         "Helper method to overcome python circular import errors"
