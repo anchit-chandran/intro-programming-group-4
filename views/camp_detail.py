a1922008520305ec7102c1488ef97eadd3cdb4dd
Yulia Trubacheva <yulitrub@gmail.com>
2023-11-26 13:24:27 +0000

    tweak widths and top container design

--- views/camp_detail.py
+++ views/camp_detail.py
@@ -153,24 +153,25 @@ class CampDetailView(BaseView):
             width=700,
             height=600,
         )
-        self.top_container.grid(row=1, column=0, padx=50, pady=20, sticky="nsew")
+        self.top_container.grid(row=1, column=0, padx=50, pady=20, sticky="nw")
 
         # info container
-        self.info_container = ttk.Frame(
+        self.info_container = tk.LabelFrame(
             master=self.top_container,
+            text="Information",
             width=300,
             height=600,
         )
-        self.info_container.grid(row=0, column=0, padx=30, pady=20, sticky="nsew")
+        self.info_container.grid(row=0, column=0, padx=30, pady=20, sticky="nw")
 
-        # header span 2 columns
-        self.info_header = ttk.Label(
-            master=self.info_container,
-            text="INFORMATION",
-        )
-        self.info_header.grid(
-            row=3, column=0, columnspan=2, pady=20, padx=10, sticky="nw"
-        )
+        # # header span 2 columns
+        # self.info_header = ttk.Label(
+        #     master=self.info_container,
+        #     text="INFORMATION",
+        # )
+        # self.info_header.grid(
+        #     row=3, column=0, columnspan=2, pady=20, padx=10, sticky="nw"
+        # )
 
         # left label
         self.location_label = ttk.Label(
@@ -186,39 +187,36 @@ class CampDetailView(BaseView):
         self.max_capacity_label.grid(row=5, column=0, sticky="w", pady=10, padx=10)
 
         # right info
-        self.location_info = ttk.Label(
+        self.location_info = tk.Entry(
             master=self.info_container,
-            text=camp_info["location"],
+            state="disabled",
+            textvariable=tk.StringVar(value=camp_info["location"]),
         )
         self.location_info.grid(row=4, column=1, sticky="w", pady=10, padx=10)
 
-        self.max_capacity_info = ttk.Label(
+        self.max_capacity_info = tk.Entry(
             master=self.info_container,
-            text=camp_info["maxCapacity"],
+            state="disabled",
+            textvariable=tk.StringVar(value=camp_info["maxCapacity"]),
         )
         self.max_capacity_info.grid(row=5, column=1, sticky="w", pady=10, padx=10)
 
         # resources container
-        self.resources_container = ttk.Frame(
+        self.resources_container = tk.LabelFrame(
             master=self.top_container,
+            text="Resources",
             width=300,
         )
-        self.resources_container.grid(row=0, column=1, padx=30, pady=10, sticky="n")
+        self.resources_container.grid(row=0, column=1, padx=30, pady=10, sticky="nw")
 
-        self.total_resources_frame = tk.LabelFrame(
-            master=self.info_container,
-            text="Resources",
-            padx=10,
-            pady=10,
-        )
         # header span 2 columns
-        self.resources_header = ttk.Label(
-            master=self.resources_container,
-            text="RESOURCES",
-        )
-        self.resources_header.grid(
-            row=3, column=1, columnspan=2, pady=20, padx=10, sticky="w"
-        )
+        # self.resources_header = ttk.Label(
+        #     master=self.resources_container,
+        #     text="RESOURCES",
+        # )
+        # self.resources_header.grid(
+        #     row=3, column=1, columnspan=2, pady=20, padx=10, sticky="w"
+        # )
 
         self.resources_num_container = ttk.Frame(
             master=self.resources_container,
@@ -241,9 +239,10 @@ class CampDetailView(BaseView):
             )
 
             # right info
-            self.resources_info = ttk.Label(
+            self.resources_info = tk.Entry(
                 master=self.resources_num_container,
-                text=resource["amount"],
+                state="disabled",
+                textvariable=tk.StringVar(value=resource["amount"]),
             )
             self.resources_info.grid(
                 row=row_number, column=2, sticky="w", pady=2, padx=10
@@ -382,7 +381,7 @@ class CampDetailView(BaseView):
         # MAKE THE TABLE SCROLLABLE
         # canvas container
         self.refugee_table_canvas = tk.Canvas(
-            master=self.all_refugees_container, width=1200, height=200
+            master=self.all_refugees_container, width=650, height=200
         )
         self.refugee_table_canvas.grid(row=1, column=0, sticky="nsew", columnspan=2)
 
@@ -403,7 +402,7 @@ class CampDetailView(BaseView):
             orient="vertical",
             command=self.refugee_table_canvas.yview,
         )
-        self.refugee_scrollbar.grid(row=1, column=2, sticky="ns")
+        self.refugee_scrollbar.grid(row=1, column=1, sticky="nse", padx=1)
 
         self.refugee_table_canvas.configure(yscrollcommand=self.refugee_scrollbar.set)
 
@@ -447,14 +446,14 @@ class CampDetailView(BaseView):
             )
             self.cell_frame.grid(row=0, column=ix, pady=5)
             add_border(self.cell_frame)
-            
+
             # Decrease width for id column
             if ix == 0:
                 column_width = 3
-            
+
             # ADD SPACE FOR LANGUAGES FOR VOLUNTEERS
             if not is_refugee_table and ix == 5:
-                column_width += 10
+                column_width += 15
 
             self.cell_content = ttk.Label(
                 master=self.cell_frame,
@@ -469,7 +468,7 @@ class CampDetailView(BaseView):
 
         # Add edit buttons
         if not header and is_refugee_table:
-            BUTTON_WIDTH = 3
+            BUTTON_WIDTH = 5
             # edit btn
             self.edit_refugees_btn = ttk.Button(
                 master=self.row_container,
