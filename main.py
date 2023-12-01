42d3021ffc12a27aba0a54053e0f6ba10c28338d
anchit-chandran <anchit97123@gmail.com>
2023-12-01 08:27:33 +0000

    refactor table to use treeview
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ class MainApplication(tk.Tk):
 
         self.current_view = None
         # Start at LoginView
-        self.switch_to_view("messages")
+        self.switch_to_view("all_plans")
 
     def switch_to_view(self, new_view: str) -> None:
         "Helper method to overcome python circular import errors"
