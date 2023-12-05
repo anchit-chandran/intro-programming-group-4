2852913ff2261f7698e83c394f36bfb5c956055e
anchit-chandran <anchit97123@gmail.com>
2023-12-05 12:26:26 +0000

    instructions
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- constants/instructions.py
+++ constants/instructions.py
@@ -1,3 +1,5 @@
+messages_instructions = "Below you can see your messages, separated by resolved and unresolved. You can scroll if there are many.\n\nYou can resolve / unresolve messages by selecting the message and pressing the appropriate button.\n\nNOTE: messages are sorted first by  Priority (highest priority at the top), then by most recently received.\n\n---Sending messages---\nYou can send message by clicking the 'New Message' button."
+
 INSTRUCTIONS = {
     "login": "",
     "plan_detail": "",
@@ -6,7 +8,7 @@ INSTRUCTIONS = {
     "add_edit_camp": "",
     "camp_detail": "",
     "all_volunteers": "",
-    "messages": "",
+    "messages": messages_instructions,
     "profile": {
         "own_profile": "Edit your own profile",
     },
