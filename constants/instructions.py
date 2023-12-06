72c419b5611f9af89b7a206de7826a65a35d7fad
Yulia Trubacheva <yulitrub@gmail.com>
2023-12-06 16:39:41 +0000

    add instructions to refugee profile

--- constants/instructions.py
+++ constants/instructions.py
@@ -12,11 +12,13 @@ new_resource = "---Input---\nCreate a new resource below.\n\nThe resource name m
 
 edit_resources = "---Input---\nPlease edit existing resource amounts in the form below.\nAdd new Resource types using '+ New resource'. Only whole number amounts are allowed.\n\n ---Submit---\nClick 'Submit changes' to save any edits.\n\nNOTE: newly added resources are already saved."
 
-camp_detail = "Information pertaining to this camp, including its Resources, Volunteers and Refugee Families can be seen here.\n\n New Refugee Families can be registered using the '+ Add Refugee Family' button.\n\n Note: If the camp has reached full capacity, new refugee registrations will not be accepted. \n\n Access details of registered refugee families that have departed using the 'View Departed Refugee' button.\n\nTo view or edit a particular Family, select the Family in the table and use the appropriate 'Selected Refugee Family Actions' action button."
+camp_detail = "Information pertaining to this camp, including its Resources, Volunteers and Refugee Families can be seen here.\n\nNew Refugee Families can be registered using the '+ Add Refugee Family' button.\n\nNote: If the camp has reached full capacity, new refugee registrations will not be accepted. \n\nAccess details of registered refugee families that have departed using the 'View Departed Refugee' button.\n\nTo view or edit a particular Family, select the Family in the table and use the appropriate 'Selected Refugee Family Actions' action button."
 
 departed_refugees = "Information about registered refugee families that have departed\n\nTo view or edit a particular Family, select the Family in the table and use the appropriate 'Selected Refugee Family Actions' action button."
 
-add_edit_refugee = "Please fill out all the fields in the form.\n\nNOTE:\n\n - age and number of adults, children and missing people must be a valid positive integer \n\n - other fields should not exceed 40 characters max limit\n\n - please enter n/a for medical condition if not applicable"
+add_edit_refugee = "Please fill out all the fields in the form.\n\nNOTE:\n\n- age and number of adults, children and missing people must be a valid positive integer \n\n- other fields should not exceed 40 characters max limit\n\n- please enter n/a for medical condition if not applicable"
+
+refugee_profile = "This page displays detailed information about the corresponding refugee family. It includes personal details and current location status.\n\nTo update the information please click the 'Edit' button \n\nUse the 'Back' button to return to the Camp Details view. \n\nNote: Editing is limited to certain fields, and changes will be reflected in the overall camp management system."
 
 INSTRUCTIONS = {
     "login": login,
@@ -30,6 +32,6 @@ INSTRUCTIONS = {
     "edit_resources": edit_resources,
     "add_edit_refugee": add_edit_refugee,
     "departed_refugees": departed_refugees,
-    "refugee_profile": "",
+    "refugee_profile": refugee_profile,
     "search": "",
 }
